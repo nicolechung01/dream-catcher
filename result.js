1f5ad56a7f2d74a3ad168e294e6a42e53b7c4ed6
Nicole Chung <nc06279@uga.edu>
2023-10-15 00:30:09 -0400

    update

--- result.js
+++ result.js
@@ -1,7 +1,7 @@
 const responseString = sessionStorage.getItem('apiResponse');
 if (responseString) {
     const resultElement = document.getElementById('api-result');
-    resultElement.textContent = response.message;
+    resultElement.textContent = responseString;
 } else {
     // Handle the case where the response is not found in session storage
     console.error('API response not found in session storage.');
